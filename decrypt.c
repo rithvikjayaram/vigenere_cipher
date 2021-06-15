87a9bf90604233e7de3d3b6b9adc5a73f3a4c486
jagriti-bhatia <jagriti.bhatia11@gmail.com>
2021-06-15 11:02:25 +0530

    modified decrypt

--- decrypt.c
+++ decrypt.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+
 void decryption(char ciphertext[], char key[])
 {
-    int ciphertext_len = strlen(ciphertext), keyLen = strlen(key), i, j;
-    char newKey[ciphertext_len], decryptedMsg[ciphertext_len];
+    int ciphertext_len = strlen(ciphertext);
+    int key_len = strlen(key);
+    int i, j;
+
+    char new_key[ciphertext_len], decrypted_msg[ciphertext_len];
 
     //generating new key
     for(i = 0, j = 0; i < ciphertext_len; ++i, ++j)
     {
-        if(j == keyLen)
+        if(j == key_len)
             j = 0;
 
-        newKey[i] = key[j];
+        new_key[i] = key[j];
     }
-    newKey[i] = '\0';
-
+    new_key[i] = '\0';
 
     for(i = 0; i < ciphertext_len; ++i)
-        decryptedMsg[i] = (((ciphertext[i] - newKey[i]) + 26) % 26) + 'A';
-    decryptedMsg[i] = '\0';
+        decrypted_msg[i] = (((ciphertext[i] - new_key[i]) + 26) % 26) + 'A';
+    decrypted_msg[i] = '\0';
 
-    printf("\nNew Generated Key: %s", newKey);
-    printf("\nDecrypted Message: %s", decryptedMsg);
-
+    printf("\nNew Generated Key: %s", new_key);
+    printf("\nDecrypted Message: %s", decrypted_msg);
 }
